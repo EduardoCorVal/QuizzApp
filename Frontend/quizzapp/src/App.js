dfea32c0a3669eb6601986fc48bd39662fe70982
Paulo Ogando Gulias <A01751587@itesm.mx>
2023-05-09 16:52:57 -0600

    Pregunta y Header

--- Frontend/quizzapp/src/App.js
+++ Frontend/quizzapp/src/App.js
@@ -1,11 +1,23 @@
-import './App.css';
-import Register from './pages/Register';
-
+import "./App.css";
+import Register from "./pages/Register";
+import Header from "./components/Header";
+import Question from "./components/Question";
+import Subtitle from "./components/Subtitle";
+import Ejemplo from "./images/ejemplo.png"
 
 function App() {
   return (
     <div className="App">
-      <Register />
+      <Header />
+      <Question
+        question="What is the capital of India?"
+        image={Ejemplo}
+        option1="Mumbai"
+        option2="Delhi"
+        option3="Kolkata"
+        option4="Chennai"
+        answer="Delhi"
+      />
     </div>
   );
 }
